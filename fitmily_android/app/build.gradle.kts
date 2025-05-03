501b62bcd95b3c8da0b98b27dc8d59dee348e8a0
남수정 <2020113388@dgu.ac.kr>
2025-05-03 21:10:02 +0900

    chore: navigation compose 추가

--- fitmily_android/app/build.gradle.kts
+++ fitmily_android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ssafy.fitmily_android"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -57,4 +57,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    // navigation
+    implementation("androidx.navigation:navigation-compose:2.8.9")
 }
