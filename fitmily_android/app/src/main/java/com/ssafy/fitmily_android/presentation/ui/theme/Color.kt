ba7a0bc4eaf1193d0fb3c9e5541392e5308a4611
임수미 <thddkwl5330@naver.com>
2025-05-04 13:35:26 +0900

    chore: 앱 색상 설정

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/theme/Color.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/theme/Color.kt
@@ -1,11 +1,24 @@
 package com.ssafy.fitmily_android.ui.theme
 
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val mainBlue = Color(0xFF359BC4)
+val secondaryBlue = Color(0xFFC8F8FF)
+val thirdBlue = Color(0xFFE3F4F7)
+val familyFirst = Color(0xFFFF7373)
+val familySecond = Color(0xFFFFBA4A)
+val familyThird = Color(0xFFFFEE00)
+val familyFourth = Color(0xFF35DD52)
+val familyFifth = Color(0xFFB6885A)
+val familySixth = Color(0xFFD074BE)
+val mainBlack = Color(0xFF141414)
+val mainWhite = Color(0xFFFFFFFF)
+val mainGray = Color(0xFFE3E3E3)
+val mainDarkGray = Color(0xFF9D9D9D)
+val backGroundGray = Color(0xFFF7F7F7)
+
+
+
