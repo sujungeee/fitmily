f2b1109de7947b52920a238bb3366f2eec601da9
김동욱 <kkdw4280322@gmail.com>
2025-05-05 14:40:08 +0900

    feat: backend 다시 생성

---
+++ fitmily_backend/user-service/src/test/java/fitmily_backend/fitmily_backend/FitmilyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fitmily_backend.fitmily_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmilyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
