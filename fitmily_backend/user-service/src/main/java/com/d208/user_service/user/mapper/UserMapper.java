4425e745adf4aef3f9f5d9deb54aa2cc03a39a35
김동욱 <kkdw4280322@gmail.com>
2025-05-06 11:36:29 +0900

    feat: yml파일 ignore 처리

--- fitmily_backend/user-service/src/main/java/com/d208/user_service/user/mapper/UserMapper.java
+++ fitmily_backend/user-service/src/main/java/com/d208/user_service/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.d208.user_service.user.mapper;
 
 
+import com.d208.user_service.user.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,6 +10,27 @@ import org.apache.ibatis.annotations.Param;
 public interface UserMapper {
 
     boolean existsByLoginId(@Param("loginId") String loginId);
+
+    // 로그인 아이디로 User 조회
+    User findByLoginId(@Param("loginId") String loginId);
+
+    // 회원 정보 삽입 (INSERT)
+    int insert(User user);
+
+    // 리프레시 토큰 업데이트
+    int updateRefreshToken(
+            @Param("userId") Integer userId,
+            @Param("refreshToken") String refreshToken
+    );
+
+    //리프레시 토큰 지우기 (로그아웃)
+    int clearRefreshToken(@Param("userId") Integer userId);
+
+    // 아이디+토큰 일치 여부 확인
+    boolean existsByIdAndRefreshToken(
+            @Param("userId") Integer userId,
+            @Param("refreshToken") String refreshToken
+    );
 
 
 }
