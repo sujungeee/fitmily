dd7f2367c7065b53a67cc6c041bf41d2b17897c4
hyuun <wotj28@naver.com>
2025-05-07 12:57:09 +0900

    style: 건강 기록 & 목표 설정 & 운동 기록 버튼들 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/MyRecordButton.kt
@@ -0,0 +1,58 @@
+package com.ssafy.fitmily_android.presentation.ui.main.my
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.ssafy.fitmily_android.ui.theme.Typography
+import com.ssafy.fitmily_android.ui.theme.mainBlack
+import com.ssafy.fitmily_android.ui.theme.mainBlue
+import com.ssafy.fitmily_android.ui.theme.mainWhite
+
+@Composable
+fun MyRecordButton(
+    iconRes: Int,
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = Modifier
+            .background(mainWhite, shape = RoundedCornerShape(16.dp))
+            .clickable { onClick() }
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = label,
+            modifier = Modifier.size(64.dp)
+        )
+        Spacer(Modifier.height(20.dp))
+        Box(
+            modifier = Modifier
+                .background(mainBlue, shape = RoundedCornerShape(8.dp))
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+        ) {
+            Text(
+                text = label,
+                color = mainWhite,
+                style = Typography.bodyMedium
+            )
+        }
+    }
+}
---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/MyRecordButtonRow.kt
@@ -0,0 +1,40 @@
+package com.ssafy.fitmily_android.presentation.ui.main.my
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ssafy.fitmily_android.R
+
+@Composable
+fun MyRecordButtonRow(
+    onHealthClick: () -> Unit,
+    onGoalClick: () -> Unit,
+    onExerciseClick: () -> Unit
+) {
+    Row(
+    modifier = Modifier
+    .fillMaxWidth(),
+    horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        MyRecordButton(
+            iconRes = R.drawable.health,
+            label = "건강 기록",
+            onClick = onHealthClick,
+            modifier = Modifier.weight(1f)
+        )
+        MyRecordButton(
+            iconRes = R.drawable.health,
+            label = "목표 설정",
+            onClick = onGoalClick,
+            modifier = Modifier.weight(1f)
+        )
+        MyRecordButton(
+            iconRes = R.drawable.health,
+            label = "운동 기록",
+            onClick = onExerciseClick,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
