d022c461ef138cf07ba44499a68c12ab0b7f93a6
hyuun <wotj28@naver.com>
2025-05-07 12:39:01 +0900

    chore: tmp 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/tmp.kt
@@ -0,0 +1,11 @@
+package com.ssafy.fitmily_android.presentation.ui.main.my
+
+import android.util.Log
+import com.ssafy.fitmily_android.presentation.ui.auth.LoginScreen
+import kotlin.math.log
+
+class tmp {
+    fun print() {
+        Log.d("임시", "임시")
+    }
+}
