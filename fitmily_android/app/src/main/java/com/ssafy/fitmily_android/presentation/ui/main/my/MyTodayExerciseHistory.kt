87628b32193d15828008207e1a80b62cdfaccaaa
hyuun <wotj28@naver.com>
2025-05-07 12:57:45 +0900

    style: 오늘의 운동 히스토리 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/MyTodayExerciseHistory.kt
@@ -0,0 +1,44 @@
+package com.ssafy.fitmily_android.presentation.ui.main.my
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ssafy.fitmily_android.ui.theme.Typography
+import com.ssafy.fitmily_android.ui.theme.mainBlack
+import com.ssafy.fitmily_android.ui.theme.mainBlue
+
+@Composable
+fun MyTodayExerciseHistory(
+    totalExerciseTime: String,
+    histories: List<ExerciseHistory>
+) {
+    Column(
+    ) {
+        Text(
+            text = "오늘의 운동 히스토리",
+            color = mainBlack,
+            style = Typography.titleLarge
+        )
+
+        Spacer(Modifier.height(12.dp))
+
+        Text(
+            text = "총 $totalExerciseTime",
+            color = mainBlue,
+            style = Typography.bodyLarge,
+            fontSize = 20.sp
+        )
+
+        Spacer(Modifier.height(12.dp))
+
+        histories.forEach { history ->
+            MyExerciseHistoryItem(history)
+            Spacer(Modifier.height(12.dp))
+        }
+    }
+}
