7ee2c1bf2f096e41f696afe4ce4f4375a2aa47c7
임수미 <thddkwl5330@naver.com>
2025-05-07 17:19:41 +0900

    feat: 뉴스 웹뷰 구현

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/home/component/NewsItem.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/home/component/NewsItem.kt
@@ -2,11 +2,13 @@ package com.ssafy.fitmily_android.presentation.ui.main.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
@@ -17,17 +19,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.ssafy.fitmily_android.R
 import com.ssafy.fitmily_android.ui.theme.thirdBlue
 
 @Composable
-fun NewsItem(item: String) {
+fun NewsItem(item: String, navController: NavHostController) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = Modifier.width(180.dp)
+            .padding(bottom = 12.dp)
+            .clickable {
+                navController.navigate("home/news")
+            }
     ){
         Image(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
                 .aspectRatio(1.8f)
                 .clip(RoundedCornerShape(16.dp)),
             contentScale = ContentScale.FillBounds,
@@ -36,7 +43,7 @@ fun NewsItem(item: String) {
         )
         Spacer(Modifier.size(12.dp))
         Text(
-            text = "뉴스 제목은 다음과 같으며 다음다음 내용이 포함됩니다.",
+            text = "$item,뉴스 제목은 다음과 같으며 다음다음 내용이 포함됩니다.",
             style = typography.bodyMedium,
             maxLines = 2
         )
