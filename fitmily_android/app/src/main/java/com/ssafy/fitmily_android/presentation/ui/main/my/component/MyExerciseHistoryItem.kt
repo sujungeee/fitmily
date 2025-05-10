2111a8f22c684eabb823b6545c464d6d654918bd
hyuun <wotj28@naver.com>
2025-05-10 11:24:38 +0900

    style: 오늘의 운동 히스토리 아이템 수정

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/component/MyExerciseHistoryItem.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/component/MyExerciseHistoryItem.kt
@@ -2,6 +2,8 @@ package com.ssafy.fitmily_android.presentation.ui.main.my.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,75 +13,74 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.ssafy.fitmily_android.R
 import com.ssafy.fitmily_android.presentation.ui.main.my.ExerciseHistory
 import com.ssafy.fitmily_android.ui.theme.Typography
 import com.ssafy.fitmily_android.ui.theme.mainBlack
+import com.ssafy.fitmily_android.ui.theme.mainBlue
+import com.ssafy.fitmily_android.ui.theme.mainDarkGray
 import com.ssafy.fitmily_android.ui.theme.mainGray
 import com.ssafy.fitmily_android.ui.theme.mainWhite
 
 @Composable
 fun MyExerciseHistoryItem(
-    history: ExerciseHistory,
-    modifier: Modifier = Modifier
+    history: ExerciseHistory
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(mainWhite, shape = RoundedCornerShape(16.dp))
-            .padding(horizontal = 20.dp, vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
+    Column(
     ) {
+        val valueText = if (history.unit != "km") {
+            history.exerciseValue.toInt()
+        } else {
+            history.exerciseValue
+        }
+
+        // 이미지
         Image(
-            painter = painterResource(id = R.drawable.health),
+            painter = painterResource(id = history.iconRes),
             contentDescription = null,
-            modifier = Modifier.size(36.dp)
+            modifier = Modifier
+                .height(120.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
         )
+        Spacer(modifier = Modifier.height(12.dp))
 
-        Spacer(Modifier.width(12.dp))
-
-        Column(
+        // 운동명
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
+            Text(
+                text = history.exerciseName,
+                style = Typography.bodyLarge,
+                color = mainBlack
+            )
+            if (history.exerciseName == "산책") {
                 Text(
-                    text = history.time,
-                    color = mainBlack,
-                    style = Typography.bodyLarge
-                )
-                Spacer(modifier = Modifier.weight(1f))
-
-                if(history.unit == "km")
-                    Text(
-                        text = "더보기",
-                        color = mainGray,
-                        style = Typography.bodyMedium
-                    )
-            }
-
-            Spacer(Modifier.height(4.dp))
-
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Spacer(modifier = Modifier.weight(1f))
-                Text(
-                    text =
-                        if(history.unit == "km")
-                            "${history.exerciseName} ${history.exerciseCount} ${history.unit}"
-                        else
-                            "${history.exerciseName} ${history.exerciseCount.toInt()} ${history.unit}",
-                    color = mainBlack,
-                    style = Typography.bodyMedium
+                    text = "더보기",
+                    style = Typography.bodySmall,
+                    color = mainDarkGray
                 )
             }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // 칼로리, 거리 칩
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            MyExerciseInfoChip(text = "${history.exerciseCalorie}kcal")
+            MyExerciseInfoChip(text = "${valueText} ${history.unit}")
         }
     }
 }
