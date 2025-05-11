21b8b81cbf02c069cb9de1678dd58c47da5d60f8
임수미 <thddkwl5330@naver.com>
2025-05-11 18:57:30 +0900

    fix: 심박수 기능 제거

--- fitmily_android/fitmily_common/src/main/java/com/ssafy/fitmily_common/TrackedData.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.fitmily_common
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TrackedData(
-    var hr: Int = 0,
-    var ibi: ArrayList<Int> = ArrayList()
-)
