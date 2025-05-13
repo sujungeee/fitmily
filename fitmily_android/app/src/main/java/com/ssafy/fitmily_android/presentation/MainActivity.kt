a6a5664f8b4780e23ea4259bb75d3b7206d55eed
남수정 <2020113388@dgu.ac.kr>
2025-05-13 10:16:25 +0900

    feat: 로그인 여부, 인증 만료 여부에 따른 네비게이션 분기 처리 기능 구현

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/MainActivity.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/MainActivity.kt
@@ -3,17 +3,54 @@ package com.ssafy.fitmily_android.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.rememberNavController
+import com.ssafy.fitmily_android.MainApplication
 import com.ssafy.fitmily_android.presentation.navigation.FitmilyNavHost
 import com.ssafy.fitmily_android.presentation.navigation.RootNavGraph
 import com.ssafy.fitmily_android.ui.theme.FitmilyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val authDataStore = MainApplication.getInstance().getDataStore()
+
         setContent {
+            val navController = rememberNavController()
+            var startGraph by remember { mutableStateOf<RootNavGraph?>(null) }
+
+            LaunchedEffect(Unit) {
+                val accessToken = authDataStore.getAccessToken()
+                startGraph = if (accessToken.isBlank()) {
+                    RootNavGraph.AuthNavGraph
+                } else {
+                    RootNavGraph.MainNavGraph
+                }
+            }
+
+            LaunchedEffect(true) {
+                authDataStore.authExpiredFlow().collect { isExpired ->
+                    if (isExpired) {
+                        navController.navigate("login") {
+                            popUpTo(0) { inclusive = true }
+                        }
+                        authDataStore.setAuthExpired(false)
+                    }
+                }
+            }
+
             FitmilyTheme {
-                // TODO: graph는 로그인 여부에 따라 추후에 달라질 예정(일단 Main으로 바로 navigate)
-                FitmilyNavHost(RootNavGraph.MainNavGraph)
+//                startGraph?.let {
+//                    FitmilyNavHost(navController, it)
+//                }
+                FitmilyNavHost(navController, RootNavGraph.MainNavGraph) // TODO: 통신 성공 시 delete
             }
         }
     }
