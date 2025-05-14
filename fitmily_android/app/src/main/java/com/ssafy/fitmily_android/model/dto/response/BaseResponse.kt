e16637f6e49ca64c12f901861d59f8a15a2a4d24
임수미 <thddkwl5330@naver.com>
2025-05-14 16:03:06 +0900

    feat: baseResponse 임시 지정

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/BaseResponse.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/BaseResponse.kt
@@ -4,5 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 open class BaseResponse<T>(
     @SerializedName("success") val success: Boolean = true,
+    @SerializedName("code") val code: Int = 200,
+    @SerializedName("message") val message: String? = null,
     @SerializedName("data") val data: T? = null
 )
