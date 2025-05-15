2e561ed6c6e2766fb39bfe9230f6c46061e310c4
hyuun <wotj28@naver.com>
2025-05-15 16:32:17 +0900

    feat: 병합 충돌 해결

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/NetworkModule.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/NetworkModule.kt
@@ -1,8 +1,8 @@
 package com.ssafy.fitmily_android.di
 
 import com.ssafy.fitmily_android.model.service.AuthService
-import com.ssafy.fitmily_android.model.service.HomeService
 import com.ssafy.fitmily_android.model.service.MyHealthService
+import com.ssafy.fitmily_android.model.service.HomeService
 import com.ssafy.fitmily_android.model.service.WalkService
 import com.ssafy.fitmily_android.network.AccessTokenInterceptor
 import com.ssafy.fitmily_android.network.ReissueInterceptor
