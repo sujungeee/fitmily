98b24121b5fa345ad1cbda540824aff51c78ab58
임수미 <thddkwl5330@naver.com>
2025-05-15 17:55:05 +0900

    feat: WalkRepositoryImpl 구현

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/repositoryimpl/WalkRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.ssafy.fitmily_android.model.repositoryimpl
+
+import com.ssafy.fitmily_android.domain.repository.WalkRepository
+import com.ssafy.fitmily_android.model.common.ApiResultHandler
+import com.ssafy.fitmily_android.model.common.Result
+import com.ssafy.fitmily_android.model.dto.request.walk.WalkEndRequest
+import com.ssafy.fitmily_android.model.dto.response.walk.WalkEndResponse
+import com.ssafy.fitmily_android.model.dto.response.walk.WalkHistoryResponse
+import com.ssafy.fitmily_android.model.dto.response.walk.WalkPathResponse
+import com.ssafy.fitmily_android.model.service.WalkService
+import jakarta.inject.Inject
+
+class WalkRepositoryImpl @Inject constructor(
+    private val walkService: WalkService
+): WalkRepository {
+    override suspend fun postWalk(request: WalkEndRequest): Result<WalkEndResponse> {
+        return ApiResultHandler.handleApi {
+            walkService.postWalk(request)
+        }
+    }
+
+    override suspend fun getWalkHistory(): Result<WalkHistoryResponse> {
+        return ApiResultHandler.handleApi {
+            walkService.getWalkHistory()
+        }
+    }
+
+    override suspend fun getWalkPath(userId: Int): Result<WalkPathResponse> {
+        return ApiResultHandler.handleApi {
+            walkService.getWalkPath(userId)
+        }
+    }
+
+    override suspend fun getWalkGoalExist(): Result<Boolean> {
+        return ApiResultHandler.handleApi {
+            walkService.getWalkGoalExist()
+        }
+    }
+
+}
