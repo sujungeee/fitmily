623bfa5163989d1032993e5d462f6afd9feb895e
임수미 <thddkwl5330@naver.com>
2025-05-15 17:54:19 +0900

    feat: RepositoryModule 추상 클래스로 변경 (bind 사용 위해)

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/RepositoryModule.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/RepositoryModule.kt
@@ -1,8 +1,13 @@
 package com.ssafy.fitmily_android.di
 
 import com.ssafy.fitmily_android.domain.repository.AuthRepository
+import com.ssafy.fitmily_android.domain.repository.HomeRepository
+import com.ssafy.fitmily_android.domain.repository.WalkRepository
 import com.ssafy.fitmily_android.model.repositoryimpl.AuthRepositoryImpl
+import com.ssafy.fitmily_android.model.repositoryimpl.HomeRepositoryImpl
+import com.ssafy.fitmily_android.model.repositoryimpl.WalkRepositoryImpl
 import com.ssafy.fitmily_android.model.service.AuthService
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,14 +16,23 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
+    @Binds
     @Singleton
-    @Provides
-    fun providesAuthRepository(
-        authService: AuthService
-    ): AuthRepository {
-        return AuthRepositoryImpl(authService)
-    }
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
 
-    // TODO: add
+    @Binds
+    @Singleton
+    abstract fun bindWalkRepository(
+        walkRepositoryImpl: WalkRepositoryImpl
+    ): WalkRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindHomeRepository(
+        homeRepositoryImpl: HomeRepositoryImpl
+    ): HomeRepository
+
 }
