7ee901f51f685a16bc0583d311d59e215177ac97
임수미 <thddkwl5330@naver.com>
2025-05-15 16:21:46 +0900

    feat: HomeRepository 생성

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/domain/repository/HomeRepository.kt
@@ -0,0 +1,27 @@
+package com.ssafy.fitmily_android.domain.repository
+
+import com.ssafy.fitmily_android.model.common.Result
+import com.ssafy.fitmily_android.model.dto.request.home.FamilyCreateRequest
+import com.ssafy.fitmily_android.model.dto.request.home.FamilyJoinRequest
+import com.ssafy.fitmily_android.model.dto.response.home.ChallengeResponse
+import com.ssafy.fitmily_android.model.dto.response.home.FamilyHealthResponse
+import com.ssafy.fitmily_android.model.dto.response.home.FamilyJoinResponse
+import com.ssafy.fitmily_android.model.dto.response.home.FamilyResponse
+import com.ssafy.fitmily_android.model.dto.response.home.FamilyTodayResponse
+
+interface HomeRepository {
+
+    suspend fun createFamily(request: FamilyCreateRequest): Result<FamilyJoinResponse>
+
+    suspend fun joinFamily(request: FamilyJoinRequest): Result<FamilyJoinResponse>
+
+    suspend fun getFamily(familyId: Int): Result<FamilyResponse>
+
+    suspend fun getDashboard(familyId: Int, today: String?): Result<FamilyTodayResponse>
+
+    suspend fun getFamilyHealth(familyId: Int): Result<FamilyHealthResponse>
+
+    suspend fun getChallenge(): Result<ChallengeResponse>
+
+    suspend fun sendPoke(userId: Int): Result<Any>
+}
