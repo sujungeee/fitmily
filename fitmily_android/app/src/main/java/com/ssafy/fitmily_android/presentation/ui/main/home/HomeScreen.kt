cf973617b6f4438f3d21c845b030eb3a126dc76c
임수미 <thddkwl5330@naver.com>
2025-05-16 16:50:00 +0900

    feat: HomeScreen 수정

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/home/HomeScreen.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/home/HomeScreen.kt
@@ -1,25 +1,27 @@
 package com.ssafy.fitmily_android.presentation.ui.main.home
 
-
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.ssafy.fitmily_android.MainApplication
 import com.ssafy.fitmily_android.presentation.ui.main.home.component.AloneHome
 import com.ssafy.fitmily_android.presentation.ui.main.home.component.FamilyHome
-import com.ssafy.fitmily_android.presentation.ui.main.walk.live.WalkLiveData
 
 private const val TAG = "HomeScreen"
 @Composable
 fun HomeScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-    var isInFamily = remember { mutableStateOf(true) }
+    val authDataStore = MainApplication.getInstance().getDataStore()
+    val homeUiState by homeViewModel.uiState.collectAsState()
 
-    if (isInFamily.value) {
+    if (homeUiState.familyId!=0) {
         FamilyHome(navController)
     } else {
         AloneHome()
