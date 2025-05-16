6965efaea568b94ac0bd975d24f12b654ee2ed1a
hyuun <wotj28@naver.com>
2025-05-16 16:49:13 +0900

    feat: MyGoalService 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/service/MyGoalService.kt
@@ -0,0 +1,32 @@
+package com.ssafy.fitmily_android.model.service
+
+import com.ssafy.fitmily_android.model.dto.request.my.MyGoalRequest
+import com.ssafy.fitmily_android.model.dto.response.my.MyGoalResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface MyGoalService {
+
+    @GET("goals")
+    suspend fun getMyGoalInfo(): Response<MyGoalResponse>
+
+    @POST("goals")
+    suspend fun insertMyGoalInfo(
+        @Body myGoalRequest: MyGoalRequest
+    ): Response<Any>
+
+    @PATCH("goals/{goalId}")
+    suspend fun patchMyGoalInfo(
+        @Path("goalId") goalId: Int, @Body myGoalRequest: MyGoalRequest
+    ): Response<Any>
+
+    @DELETE("goals/{goalId}")
+    suspend fun deleteMyGoalInfo(
+        @Path("goalId") goalId: Int
+    ): Response<Any>
+}
