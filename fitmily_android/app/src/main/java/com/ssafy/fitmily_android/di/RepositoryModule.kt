f7b9355326dda7055cfcf879b10e15a13da985e5
hyuun <wotj28@naver.com>
2025-05-16 15:20:50 +0900

    feat: WeatherRepository 의존성 주입

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/RepositoryModule.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/di/RepositoryModule.kt
@@ -2,10 +2,13 @@ package com.ssafy.fitmily_android.di
 
 import com.ssafy.fitmily_android.domain.repository.AuthRepository
 import com.ssafy.fitmily_android.domain.repository.MyHealthRepository
+import com.ssafy.fitmily_android.domain.repository.WeatherRepository
 import com.ssafy.fitmily_android.model.repositoryimpl.AuthRepositoryImpl
 import com.ssafy.fitmily_android.model.repositoryimpl.MyHealthRepositoryImpl
+import com.ssafy.fitmily_android.model.repositoryimpl.WeatherRepositoryImpl
 import com.ssafy.fitmily_android.model.service.AuthService
 import com.ssafy.fitmily_android.model.service.MyHealthService
+import com.ssafy.fitmily_android.model.service.WeatherService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,5 +32,13 @@ object RepositoryModule {
         myHealthService: MyHealthService
     ): MyHealthRepository {
         return MyHealthRepositoryImpl(myHealthService)
+    }
+
+    @Singleton
+    @Provides
+    fun providesWeatherRepository(
+        weatherService: WeatherService
+    ): WeatherRepository {
+        return WeatherRepositoryImpl(weatherService)
     }
 }
