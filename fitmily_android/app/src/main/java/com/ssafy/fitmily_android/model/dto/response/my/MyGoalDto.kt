78f0c1bbc4f0ea858a96624932994f323fee6753
hyuun <wotj28@naver.com>
2025-05-16 16:48:59 +0900

    feat: MyGoalResponse 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/my/MyGoalDto.kt
@@ -0,0 +1,7 @@
+package com.ssafy.fitmily_android.model.dto.response.my
+
+data class MyGoalDto(
+    val exerciseGoalName: String,
+    val exerciseGoalValue: Double,
+    val exerciseRecordValue: Double
+)
