24755a84ade7ec74afbbd985a65ef9e6fc8c2e60
김동욱 <kkdw4280322@gmail.com>
2025-05-17 21:17:09 +0900

    feat: 산책 기록 조회 mapper추가

--- fitmily_backend/user-service/src/main/java/com/d208/fitmily/domain/walk/mapper/WalkMapper.java
+++ fitmily_backend/user-service/src/main/java/com/d208/fitmily/domain/walk/mapper/WalkMapper.java
@@ -1,5 +1,6 @@
 package com.d208.fitmily.domain.walk.mapper;
 
+import com.d208.fitmily.domain.exercise.dto.ExerciseRecordResponseDto;
 import com.d208.fitmily.domain.walk.dto.StopWalkDto;
 import com.d208.fitmily.domain.walk.dto.WalkResponseDto;
 import org.apache.ibatis.annotations.*;
@@ -21,7 +22,7 @@ public interface WalkMapper {
     @Options(useGeneratedKeys = true, keyProperty = "walkId")
     int insertStopWalk(StopWalkDto walk);
 
-    // 2. 산책 기록 조회 (동적 쿼리)
+    // 2. 산책 기록 조회
     @Select("""
         <script>
         SELECT
@@ -56,4 +57,17 @@ public interface WalkMapper {
         )
         """)
     Boolean walkGoalExists(@Param("userId") Integer userId);
+
+
+    @Select("""
+    SELECT
+        w.walk_id AS walkId,
+        w.walk_route_img AS imgUrl,
+        w.walk_calories AS exerciseCalories,
+        w.walk_distance AS exerciseRecord
+    FROM walk w
+    WHERE w.user_id = #{userId}
+      AND DATE(w.walk_start_time) = CURDATE()
+""")
+    List<ExerciseRecordResponseDto> findTodayWalkRecords(Integer userId);
 }
