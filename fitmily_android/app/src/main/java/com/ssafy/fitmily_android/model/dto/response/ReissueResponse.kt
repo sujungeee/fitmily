a3e2e1a807576c4515e7717472333bab8f1d9c87
남수정 <2020113388@dgu.ac.kr>
2025-05-17 11:47:47 +0900

    delete: reissue 관련 파일 삭제

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/ReissueResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.ssafy.fitmily_android.model.dto.response
-
-import com.google.gson.annotations.SerializedName
-
-data class ReissueResponse(
-    @SerializedName("accessToken") val accessToken: String
-    , @SerializedName("refreshToken") val refreshToken: String
-)
