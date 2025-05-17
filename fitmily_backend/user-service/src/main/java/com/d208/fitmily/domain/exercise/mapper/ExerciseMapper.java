ef351fa838e30d48c980b7805d19bc2fbcaf346b
김동욱 <kkdw4280322@gmail.com>
2025-05-17 15:51:28 +0900

    feat: 운동 달성률 업데이트 로직 추가

--- fitmily_backend/user-service/src/main/java/com/d208/fitmily/domain/exercise/mapper/ExerciseMapper.java
+++ fitmily_backend/user-service/src/main/java/com/d208/fitmily/domain/exercise/mapper/ExerciseMapper.java
@@ -1,11 +1,11 @@
 package com.d208.fitmily.domain.exercise.mapper;
 
+import com.d208.fitmily.domain.exercise.dto.ExerciseRecordInsertDto;
 import com.d208.fitmily.domain.exercise.entity.Exercise;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ExerciseMapper {
@@ -22,9 +22,47 @@ public interface ExerciseMapper {
     int calculateUserTotalCalories(@Param("userId") int userId, @Param("date") String date);
 
 
+    //    운동 기록 추가
+    @Insert("""
+        INSERT INTO exercise_record (
+            user_id,
+            exercise_name,
+            exercise_record,
+            exercise_time,
+            exercise_calories
+        ) VALUES (
+            #{userId},
+            #{exerciseName},
+            #{exerciseRecord},
+            #{exerciseTime},
+            #{exerciseCalories}
+        )
+    """)
+    void insertExerciseRecord(ExerciseRecordInsertDto dto);
+
+    @Select("""
+    SELECT g.goal_value AS goalValue,
+           COALESCE(SUM(r.exercise_record), 0) AS todayTotal
+    FROM exercise_goal g LEFT JOIN exercise_record r ON g.user_id = r.user_id
+                               AND g.exercise_goal_name = r.exercise_name
+                               AND DATE(r.created_at) = CURDATE()
+    WHERE g.user_id = #{userId}
+      AND g.exercise_goal_name = #{exerciseName}
+    GROUP BY g.goal_value
+    """)
+    Map<String, Integer> findGoalAndTodayTotal(@Param("userId") Integer userId, @Param("exerciseName") String exerciseName);
 
 
-    
+    @Update("""
+    UPDATE exercise_goal
+    SET progress_value = #{progressValue},
+        progress_rate = #{progressRate}
+    WHERE user_id = #{userId}
+      AND exercise_goal_name = #{exerciseName}
+    """)
+    void updateProgress(@Param("userId") Integer userId, @Param("exerciseName") String exerciseName, @Param("progressValue") Integer progressValue, @Param("progressRate") Integer progressRate);
+
+
 
 
 
