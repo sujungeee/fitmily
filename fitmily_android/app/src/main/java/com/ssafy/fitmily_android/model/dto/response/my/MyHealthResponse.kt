02c6c62e4619b1de472cfcb3ac562b8cc2c1e635
hyuun <wotj28@naver.com>
2025-05-18 14:08:39 +0900

    fix: MyHealthResponse 칼럼명 수정

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/my/MyHealthResponse.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/my/MyHealthResponse.kt
@@ -3,10 +3,10 @@ package com.ssafy.fitmily_android.model.dto.response.my
 data class MyHealthResponse(
     val bmi: Float,
     val createdAt: String,
-    val fiveMajorDiseases: List<String>,
+    val fiveMajorDiseasesList: List<String>,
     val healthId: Int,
     val height: Float,
-    val otherDiseases: List<String>,
+    val otherDiseasesList: List<String>,
     val updatedAt: String,
     val weight: Float
 )
