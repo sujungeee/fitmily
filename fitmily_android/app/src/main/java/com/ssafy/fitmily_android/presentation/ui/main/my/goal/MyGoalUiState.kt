b26af3dfb7d1dae3f4ef87fe357cb2df6934c8e1
hyuun <wotj28@naver.com>
2025-05-18 22:49:50 +0900

    feat: MyGoalUiState 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/my/goal/MyGoalUiState.kt
@@ -0,0 +1,7 @@
+package com.ssafy.fitmily_android.presentation.ui.main.my.goal
+
+import com.ssafy.fitmily_android.model.dto.response.my.MyGoalResponse
+
+data class MyGoalUiState(
+    val myGoalInfo: MyGoalResponse? = null
+)
