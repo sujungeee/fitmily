d3961a9763c50d30da0d1a8dc5ae5287bd8d237a
hyuun <wotj28@naver.com>
2025-05-18 22:33:38 +0900

    feat: MyGoalRepositoryImpl 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/repositoryimpl/MyGoalRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.ssafy.fitmily_android.model.repositoryimpl
+
+import com.ssafy.fitmily_android.domain.repository.MyGoalRepository
+import com.ssafy.fitmily_android.model.common.ApiResultHandler
+import com.ssafy.fitmily_android.model.dto.response.my.MyGoalResponse
+import com.ssafy.fitmily_android.model.service.MyGoalService
+import com.ssafy.fitmily_android.model.common.Result
+import com.ssafy.fitmily_android.model.dto.request.my.MyGoalRequest
+import javax.inject.Inject
+
+class MyGoalRepositoryImpl @Inject constructor(
+    private val myGoalService: MyGoalService
+): MyGoalRepository {
+
+    override suspend fun getMyGoalInfo(): Result<MyGoalResponse> {
+        return ApiResultHandler.handleApi {
+            myGoalService.getMyGoalInfo()
+        }
+    }
+
+    override suspend fun insertMyGoalInfo(
+        exerciseGoalName: String,
+        exerciseGoalValue: Float
+    ): Result<Unit> {
+        return ApiResultHandler.handleApi {
+            val myGoalRequest = MyGoalRequest(
+                exerciseGoalName = exerciseGoalName,
+                exerciseGoalValue = exerciseGoalValue
+            )
+            myGoalService.insertMyGoalInfo(myGoalRequest)
+        }
+    }
+
+    override suspend fun patchMyGoalInfo(
+        goalId: Int,
+        exerciseGoalValue: Float
+    ): Result<Unit> {
+        return ApiResultHandler.handleApi {
+            val myGoalRequest = MyGoalRequest(
+                exerciseGoalValue = exerciseGoalValue
+            )
+            myGoalService.patchMyGoalInfo(
+                goalId = goalId,
+                myGoalRequest = myGoalRequest
+            )
+        }
+    }
+
+    override suspend fun deleteMyGoalInfo(goalId: Int): Result<Unit> {
+        return ApiResultHandler.handleApi {
+            myGoalService.deleteMyGoalInfo(goalId = goalId)
+        }
+    }
+
+}
