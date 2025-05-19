4cf9a1d5a6649e6334c7f83e96f0c22dc437eb6a
김동욱 <kkdw4280322@gmail.com>
2025-05-19 10:03:16 +0900

    feat: handler 안쓰는 파일 주석

--- fitmily_backend/user-service/src/main/java/com/d208/fitmily/global/handler/StompPrincipal.java
+++ fitmily_backend/user-service/src/main/java/com/d208/fitmily/global/handler/StompPrincipal.java
@@ -1,24 +1,24 @@
-package com.d208.fitmily.global.handler;
-
-import lombok.RequiredArgsConstructor;
-
-import java.security.Principal;
-
-
-import java.io.Serializable;
-import java.security.Principal;
-
-public class StompPrincipal implements Principal, Serializable {
-
-    private static final long serialVersionUID = 1L;
-    private final String name;
-
-    public StompPrincipal(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-}
+//package com.d208.fitmily.global.handler;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//import java.security.Principal;
+//
+//
+//import java.io.Serializable;
+//import java.security.Principal;
+//
+//public class StompPrincipal implements Principal, Serializable {
+//
+//    private static final long serialVersionUID = 1L;
+//    private final String name;
+//
+//    public StompPrincipal(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return name;
+//    }
+//}
