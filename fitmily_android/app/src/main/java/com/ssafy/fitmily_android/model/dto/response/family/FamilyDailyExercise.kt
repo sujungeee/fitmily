9b8ede7bbe58f2bfff4b521d77dfdb7b17a4acb6
hyuun <wotj28@naver.com>
2025-05-19 15:34:30 +0900

    feat: FamilyDailyResponse 추가

---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/family/FamilyDailyExercise.kt
@@ -0,0 +1,11 @@
+package com.ssafy.fitmily_android.model.dto.response.family
+
+data class FamilyDailyExercise(
+    val exerciseCalories: Int,
+    val exerciseCount: Double,
+    val exerciseGoalValue: Int,
+    val exerciseId: Int,
+    val exerciseName: String,
+    val exerciseRouteImg: String,
+    val exerciseTime: Int
+)
---
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/dto/response/family/FamilyDailyResponse.kt
@@ -0,0 +1,5 @@
+package com.ssafy.fitmily_android.model.dto.response.family
+
+data class FamilyDailyResponse(
+    val members: List<FamilyDailyMember>
+)
