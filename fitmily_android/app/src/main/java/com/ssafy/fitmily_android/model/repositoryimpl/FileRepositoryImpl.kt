2211f2e3c65d490271bb2ca047ca192e90c2c3bc
남수정 <2020113388@dgu.ac.kr>
2025-05-20 10:12:12 +0900

    fix: presignedurl 응답 형식 변경

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/repositoryimpl/FileRepositoryImpl.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/model/repositoryimpl/FileRepositoryImpl.kt
@@ -1,25 +1,18 @@
 package com.ssafy.fitmily_android.model.repositoryimpl
 
 import com.ssafy.fitmily_android.domain.repository.FileRepository
-import com.ssafy.fitmily_android.domain.repository.WalkRepository
 import com.ssafy.fitmily_android.model.common.ApiResultHandler
 import com.ssafy.fitmily_android.model.common.Result
-import com.ssafy.fitmily_android.model.dto.request.walk.WalkEndRequest
-import com.ssafy.fitmily_android.model.dto.response.walk.WalkEndResponse
-import com.ssafy.fitmily_android.model.dto.response.walk.WalkHistoryResponse
-import com.ssafy.fitmily_android.model.dto.response.walk.WalkPathResponse
+import com.ssafy.fitmily_android.model.dto.request.file.PresignedUrlRequest
 import com.ssafy.fitmily_android.model.service.FileService
-import com.ssafy.fitmily_android.model.service.WalkService
 import jakarta.inject.Inject
 
 class FileRepositoryImpl @Inject constructor(
     private val fileService: FileService
 ): FileRepository {
-
-    override suspend fun getPresignedUrl(fileName: String, contentType: String): Result<Unit> {
+    override suspend fun getPresignedUrl(fileName: String, contentType: String): Result<String> {
         return ApiResultHandler.handleApi {
-            fileService.getPresignedUrl(fileName, contentType)
+            fileService.getPresignedUrl(PresignedUrlRequest(fileName, contentType))
         }
     }
-
 }
