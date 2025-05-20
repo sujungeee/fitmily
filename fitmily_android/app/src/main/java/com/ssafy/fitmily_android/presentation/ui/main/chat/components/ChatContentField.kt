8e504700a788f16270e5bfc1958848213a1c7740
남수정 <2020113388@dgu.ac.kr>
2025-05-20 10:16:29 +0900

    feat: 기존, 웹 소켓 채팅 불러오기 / 스크롤 기능 구현

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/chat/components/ChatContentField.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/chat/components/ChatContentField.kt
@@ -14,6 +14,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.ssafy.fitmily_android.MainApplication
+import com.ssafy.fitmily_android.model.dto.response.chat.ChatMessage
 import com.ssafy.fitmily_android.presentation.ui.main.chat.ChatViewModel
 import com.ssafy.fitmily_android.util.ProfileUtil
 
@@ -22,21 +23,35 @@ fun ChatContentField (
     modifier : Modifier
     , chatViewModel: ChatViewModel = hiltViewModel()
 ) {
-    val scrollState = rememberLazyListState()
     val uiState by chatViewModel.chatUiState.collectAsStateWithLifecycle()
+
+    var userId by remember { mutableStateOf(-1) }
+    val scrollState = rememberLazyListState()
+    var isScrolled by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        userId = MainApplication.getInstance().getDataStore().getUserId()
+        // 기존 채팅 불러오기
+        chatViewModel.getChatListFlow()
+    }
 
     val chatItems = uiState.chatPagingData.collectAsLazyPagingItems()
     val serverChatSize = chatItems.itemCount
     val socketChatSize = uiState.newMessages.size
     val totalChatSize = serverChatSize + socketChatSize
 
-    var userId by remember { mutableStateOf(-1) }
-    LaunchedEffect(Unit) {
-        userId = MainApplication.getInstance().getDataStore().getUserId()
+    // 처음 진입 시, 맨 아래로 스크롤
+    LaunchedEffect(serverChatSize) {
+        if (serverChatSize > 0 && !isScrolled) {
+            scrollState.scrollToItem(serverChatSize - 1)
+            isScrolled = true
+        }
     }
 
-    if (totalChatSize > 0) {
-        LaunchedEffect(totalChatSize) {
+    // 새 채팅이 왔을 시, 맨 아래에 있으면 다시 맨 아래로 스크롤
+    LaunchedEffect (socketChatSize){
+        val isBottom = scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == totalChatSize - 1
+        if (socketChatSize > 0 && isBottom) {
             scrollState.scrollToItem(totalChatSize - 1)
         }
     }
@@ -46,23 +61,39 @@ fun ChatContentField (
         modifier = modifier
         , state = scrollState
     ) {
+        // 기존 채팅
         items(chatItems.itemCount) { index ->
-            val chat = ChatMessage(
-                profileColor = ProfileUtil().seqToColor(chatItems[index]!!.senderInfo.familySequence)!!
-                , profileIcon = chatItems[index]!!.senderInfo.userZodiacName
-                , nickname = chatItems[index]!!.senderInfo.userNickname
-                , message = chatItems[index]!!.contentInfo.text
-                , imageUrl = chatItems[index]!!.contentInfo.imageUrl
-                , unReadCount = chatItems[index]!!.unReadCount
-                , date = chatItems[index]!!.sendAt
-            )
+            val chat = toChatItem(chatItems[index]!!)
             if (chatItems[index]?.senderId == userId) {
                 MyChatMessageItem(chat)
             } else {
                 OthersChatMessageItem(chat)
             }
         }
+
+        // 웹소켓 채팅
+        items(uiState.newMessages.size) { index ->
+            val newChat = uiState.newMessages[index].message
+            val chat = toChatItem(newChat)
+            if (newChat.senderId == userId) {
+                MyChatMessageItem(chat)
+            } else {
+                OthersChatMessageItem(chat)
+            }
+        }
     }
+}
+
+fun toChatItem(chatMessage: ChatMessage): com.ssafy.fitmily_android.presentation.ui.main.chat.components.ChatMessage {
+    return ChatMessage(
+        profileColor = ProfileUtil().seqToColor(chatMessage.senderInfo.familySequence)!!
+        , profileIcon = chatMessage.senderInfo.userZodiacName
+        , nickname = chatMessage.senderInfo.userNickname
+        , message = chatMessage.contentInfo.text
+        , imageUrl = chatMessage.contentInfo.imageUrl
+        , unReadCount = chatMessage.unreadCount
+        , date = chatMessage.sentAt
+    )
 }
 
 data class ChatMessage(
