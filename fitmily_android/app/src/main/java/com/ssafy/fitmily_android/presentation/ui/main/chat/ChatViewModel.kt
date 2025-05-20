54fee62d552dfb6c742ac4f4d175b6e770f091b1
남수정 <2020113388@dgu.ac.kr>
2025-05-20 09:58:47 +0900

    fix: file 관련 context 추가

--- fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/chat/ChatViewModel.kt
+++ fitmily_android/app/src/main/java/com/ssafy/fitmily_android/presentation/ui/main/chat/ChatViewModel.kt
@@ -1,6 +1,8 @@
 package com.ssafy.fitmily_android.presentation.ui.main.chat
 
+import android.content.Context
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -9,6 +11,9 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.ssafy.fitmily_android.MainApplication
 import com.ssafy.fitmily_android.domain.usecase.chat.ChatListUseCase
+import com.ssafy.fitmily_android.domain.usecase.home.GetFamilyUseCase
+import com.ssafy.fitmily_android.domain.usecase.s3.S3UseCase
+import com.ssafy.fitmily_android.domain.usecase.walk.GetPresignedUrlUseCase
 import com.ssafy.fitmily_android.model.dto.request.chat.ChatSendRequest
 import com.ssafy.fitmily_android.network.ChatManager
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -21,18 +26,26 @@ import org.hildan.krossbow.stomp.conversions.moshi.withMoshi
 import org.hildan.krossbow.stomp.frame.StompFrame
 import org.hildan.krossbow.stomp.headers.StompSendHeaders
 import org.hildan.krossbow.stomp.headers.StompSubscribeHeaders
-import com.ssafy.fitmily_android.model.common.Result
 import com.ssafy.fitmily_android.model.dto.response.chat.ChatMessage
+import com.ssafy.fitmily_android.model.dto.response.chat.WSChatResponse
+import com.ssafy.fitmily_android.util.FileUtil
+import com.ssafy.fitmily_android.util.ViewModelResultHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.asRequestBody
 
 private const val TAG = "ChatViewModel_fitmily"
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-    private val chatListUseCase: ChatListUseCase
+    private val getFamilyUseCase: GetFamilyUseCase
+    , private val chatListUseCase: ChatListUseCase
+    , private val getPresignedUrlUseCase: GetPresignedUrlUseCase
+    , private val s3UseCase: S3UseCase
 ) : ViewModel () {
+
     private val _chatUiState = MutableStateFlow(ChatUiState())
     val chatUiState: StateFlow<ChatUiState> = _chatUiState
 
@@ -41,32 +54,34 @@ class ChatViewModel @Inject constructor(
     private val chatClient = ChatManager()
     private lateinit var stompSession: StompSession
     private lateinit var chatTopic: Flow<StompFrame.Message>
-    val authDataStore = MainApplication.getInstance().getDataStore()
-
     var chatList: Flow<PagingData<ChatMessage>> ?= null
+
+    val authDataStore = MainApplication.getInstance().getDataStore()
 
     fun initStomp() {
         viewModelScope.launch {
             runCatching {
-//                familyId = authDataStore.getFamilyId()
-                familyId = 1.toString() // TODO: delete
+                familyId = authDataStore.getFamilyId().toString()
 
                 stompSession = chatClient.stompClient.connect(
                     url = chatClient.url
-                    , customStompConnectHeaders = mapOf("Authorization" to "tmpAccessToken") // TODO: edit to authDataStore.getAccessToken()
+                    , customStompConnectHeaders = mapOf("Authorization" to "Bearer ${authDataStore.getAccessToken()}")
                 ).withMoshi(chatClient.moshi)
+                Log.d(TAG, "initStomp: stompSession: ${stompSession}")
 
                 // 채팅방 구독
                 chatTopic = stompSession.subscribe(
                     StompSubscribeHeaders (
                         destination = "$SUBSCRIBE_URL$familyId"
-                        , customHeaders = mapOf("Authorization" to "tmpAccessToken")
+                        , customHeaders = mapOf("Authorization" to "Bearer ${authDataStore.getAccessToken()}")
                     )
                 )
+                Log.d(TAG, "initStomp: chatTopic: ${chatTopic}")
 
                 // 채팅 수신
                 chatTopic.collect {
-                    val newMessage = chatClient.moshi.adapter(ChatMessage::class.java).fromJson(it.bodyAsText)
+                    val newMessage = chatClient.moshi.adapter(WSChatResponse::class.java).fromJson(it.bodyAsText)
+                    Log.d(TAG, "initStomp: newMessage: ${newMessage}")
                     _chatUiState.update { state ->
                         state.copy(newMessages = state.newMessages + newMessage!!)
                     }
@@ -75,42 +90,92 @@ class ChatViewModel @Inject constructor(
         }
     }
 
-    fun cancelStomp() {
-        viewModelScope.launch {
-            stompSession.disconnect()
+    override fun onCleared() {
+        super.onCleared()
+        if (::stompSession.isInitialized) {
+            try {
+                runBlocking { stompSession.disconnect() }
+                Log.d(TAG, "cancelStomp: ")
+            } catch (e: Exception) {
+                Log.e(TAG, "cancelStomp: ${e.message}")
+            }
         }
     }
 
     // 채팅 전송
-    fun sendMessage(type: String, message: String? = "", images: List<Uri>? = listOf()) {
+    fun sendMessage(context: Context, type: String, message: String? = "", images: List<Uri>? = listOf()) {
         viewModelScope.launch {
             runCatching {
-                if (type.equals("image")) { // 이미지 전송(하나씩)
+                if (type.equals("image")) {
+                    // 이미지 전송(하나씩)
                     for (image in images!!) {
-                        // TODO: presigned url -> S3 upload
-
-
+                        val s3Image = imageUpload(context, image)
+                        Log.d(TAG, "sendMessage: s3Image: ${s3Image}")
                         stompSession.withMoshi(chatClient.moshi).convertAndSend(
                             StompSendHeaders(
                                 destination = "$SEND_URL$familyId",
-                                customHeaders = mapOf("Authorization" to "tmpAccessToken")
+                                customHeaders = mapOf("Authorization" to "Bearer ${authDataStore.getAccessToken()}")
                             )
-                            , ChatSendRequest(type, "", image.toString())
+                            , ChatSendRequest(type, "", s3Image)
                         )
+                        Log.d(TAG, "sendMessage: 이미지 전송")
                     }
-                } else { // 메시지 전송
+                } else {
+                    // 메시지 전송
                     stompSession.withMoshi(chatClient.moshi).convertAndSend(
                         StompSendHeaders(
                             destination = "$SEND_URL$familyId",
-                            customHeaders = mapOf("Authorization" to "tmpAccessToken")
+                            customHeaders = mapOf("Authorization" to "Bearer ${authDataStore.getAccessToken()}")
                         )
                         , ChatSendRequest(type, message, "")
                     )
+                    Log.d(TAG, "sendMessage: 텍스트 전송")
                 }
             }
         }
     }
 
+    // 이미지 업로드
+    suspend fun imageUpload(context: Context, image: Uri): String {
+        runCatching {
+            Log.d(TAG, "imageUpload: 함수 발동")
+            val fileName = FileUtil().getFileName(context, image)
+            val fileExtension = FileUtil().getFileExtension(context, image)
+            val contentType = when(fileExtension) {
+                "jpg" -> "image/jpeg"
+                "jpeg" -> "image/jpeg"
+                "png" -> "image/png"
+                else -> null
+            }
+            val getPresignedUrlResult = getPresignedUrlUseCase(fileName!!, contentType!!)
+            ViewModelResultHandler.handle(
+                result = getPresignedUrlResult
+                , onSuccess = { data ->
+                    Log.d(TAG, "imageUpload: presignedUrl: ${data}")
+                    // upload
+                    val file = FileUtil().getFileFromUri(context, image, fileName, fileExtension!!)
+                    val requestBody = file.asRequestBody(contentType.toMediaType())
+                    val uploadResult = s3UseCase(data!!, requestBody)
+                    ViewModelResultHandler.handle(
+                        result = uploadResult
+                        , onSuccess = {
+                            Log.d(TAG, "imageUpload: 성공")
+                            return fileName
+                        }
+                        , onError = { msg ->
+                            Log.d(TAG, "imageUpload: presignedUrl: ${msg}")
+                        }
+                    )
+                }
+                , onError = { msg ->
+                    Log.d(TAG, "getPresignedUrl: ${msg}")
+                }
+            )
+        }
+        return ""
+    }
+
+    // 기존 채팅 불러오기
     fun getChatListFlow() {
         chatList = Pager(
             config = PagingConfig(pageSize = 20),
@@ -122,6 +187,27 @@ class ChatViewModel @Inject constructor(
 
         _chatUiState.update {
             it.copy(chatPagingData = chatList!!)
+        }
+    }
+
+    // 가족 정보 불러오기
+    fun getFamilyInfo(familyId: Int) {
+        viewModelScope.launch {
+            val result = getFamilyUseCase(familyId)
+            ViewModelResultHandler.handle(
+                result = result
+                , onSuccess = { data ->
+                    _chatUiState.update {
+                        it.copy(
+                            familyName = data!!.familyName
+                            , familyCount = data.familyPeople
+                        )
+                    }
+                }
+                , onError = { msg ->
+                    Log.d(TAG, "getFamilyInfo: ${msg}")
+                }
+            )
         }
     }
 
