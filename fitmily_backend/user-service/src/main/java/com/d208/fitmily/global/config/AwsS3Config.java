2bc0aa43ddaa2cb1a4a09b1fdc90bf134c5bbf2f
김동욱 <kkdw4280322@gmail.com>
2025-05-20 15:48:10 +0900

    feat: aws s3 오류 수정중

--- fitmily_backend/user-service/src/main/java/com/d208/fitmily/global/config/AwsS3Config.java
+++ fitmily_backend/user-service/src/main/java/com/d208/fitmily/global/config/AwsS3Config.java
@@ -15,7 +15,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Component
 @ConfigurationProperties(prefix = "cloud.aws")
 @Getter
 @Setter
